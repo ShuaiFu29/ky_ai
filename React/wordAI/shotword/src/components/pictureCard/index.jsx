7c509830957ed271460e680044d881485af33469
kangyi <1729166956@qq.com>
2025-07-03 21:37:21 +0800

    图片识别学习

--- React/wordAI/shotword/src/components/pictureCard/index.jsx
+++ React/wordAI/shotword/src/components/pictureCard/index.jsx
@@ -4,23 +4,28 @@ import React, {
 
 import './style.css'
 
-const PictureCard = () => {
+const PictureCard = (props) => {
+  // console.log(props, '////')
+  const { uploadImage, word } = props
+  // console.log(uploadImage, '+++++')
   const [imgPreview, setImgPreview] = useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png')
-  const [word, setWord] = useState('')
 
   const updateImageData = (e) => {
     // html5 的文件上传功能   ?. 可选链操作符
     const file = e.target.files?.[0]
     if (!file) return
-    console.log(file)
+    // console.log(file)
     // 图片预览  I/0 操作
     return new Promise((resolve, reject) => {
       const reader = new FileReader()
       reader.readAsDataURL(file)
       reader.onload = (e) => {
-        console.log(reader.result)
+        // console.log(reader.result)
         // 响应式业务
         setImgPreview(reader.result)
+        // 如何将图片数据交给父组件
+        uploadImage(reader.result)
+        resolve(reader.result)
       }
     })
   }
