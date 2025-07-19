0f004704010210e54eae06c6ab03fbd60d65dd42
kangyi <1729166956@qq.com>
2025-07-19 16:42:31 +0800

    babel、受控组件和非受控组件学习

---
+++ React/Interview/compile_jsx/1.jsx
@@ -0,0 +1,16 @@
+const element = <h1>Hello, world!</h1>
+
+const element2 = (
+  <ul>
+    <li key='ds1'>1</li>
+    <li key='ds2'>2</li>
+    <li key='d3'>3</li>
+  </ul>
+)
+
+let a = 1
+const element3 = (
+  <div>
+    {a + 1}
+  </div>
+)
---
+++ React/Interview/compile_jsx/2.jsx
@@ -0,0 +1,10 @@
+const element = /*#__PURE__*/React.createElement("h1", null, "Hello, world!");
+const element2 = /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", {
+  key: "ds1"
+}, "1"), /*#__PURE__*/React.createElement("li", {
+  key: "ds2"
+}, "2"), /*#__PURE__*/React.createElement("li", {
+  key: "d3"
+}, "3"));
+let a = 1;
+const element3 = /*#__PURE__*/React.createElement("div", null, a + 1);
