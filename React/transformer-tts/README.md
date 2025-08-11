626540b0d3eeefe8cb64a7d00844578cd875e664
kangyi <1729166956@qq.com>
2025-08-11 17:01:59 +0800

    语音合成初学习

--- React/transformer-tts/README.md
+++ React/transformer-tts/README.md
@@ -9,9 +9,13 @@
     - 使用transformer.js 的端模型
     - 使用taildwindcss 原子CSS 几乎不需要些样式
         类名文档语义很好，特别适合AI  生成
+        高效解决适配 w-full + max-w-xl
     - webworker nlp 任务
         1. 延迟加载大模型
         2. 在现在组件后再实例化 性能好
         3. 卸载时移除事件
     - 封装组件
 - 项目的难点
+    - 单例模式封装  MyTextToSpeechPipeline
+    - getInstance 只实例化一次
+    - 懒执行
--- React/transformer-tts/src/worker.js
+++ React/transformer-tts/src/worker.js
@@ -1,14 +1,47 @@
-// transformer.js 文本 -> 语音 tts
-
-
 import {
-  env
+  env,    // 配置AI 模型运行环境
+  Tensor,    // AI 模型处理数据的基本单位
+  AutoTokenizer,  // AI 自行分词器
+  SpeechT5ForTextToSpeech,   // 文本转语音模型
+  SpeechT5HifiGan     // 语音合成模型 和音色合成
 } from '@xenova/transformers'
+// huggingFace  开源大模型社区
+// 禁用本地大模型，去请求远程的   tts 模型
+env.allowLocalModels = false
+// transformer.js  文本-》语音 tts
 
+// 单例模式   核心难点
+// 多次执行tts ai 业务 但是只会实例化一次
+// 它的实例化开销太大了，也没有必要
+class MyTextToSpeechPipeline {
+  // AI 语音模型的数据源地址  用于下载不同说话人的声音特性向量
+  // 每个字，每个词
+  static BASE_URL = 'https://huggingface.co/datasets/Xenova/cmu-arctic-xvectors-extracted/resolve/main/';
+  // 文本 -> speecht5_tts  语音特征
+  static model_id = 'Xenova/speecht_5_tts'
+  // 语音特征 -> speecht5_hifigan -> 特有的角色音频文件 
+  static vocoder_id = 'Xenova/sppecht5_hifigan'
+  // 分词器实例
+  static tokenizer_instance = null
+  // 模型实例
+  static model_instance = null
+  // 合成实例
+  static vocoder_instance = null
+  static async getInstance(progress_callback = null) {
+    if (this.tokenizer_instance === null) {
+      // 之前处理过的大模型，被预先训练过的
+      this.tokenizer = AutoTokenizer.from_pretrained(this.model_id, {
+        progress_callback
+      })
+    }
+  }
+}
 
-
-
-
-self.onmessage = (e) => {
-  console.log(e)
+self.onmessage = async (e) => {
+  // console.log(e)
+  // AI pipeline 派发一个nlp 任务
+  // 懒加载 llm 初始化和懒加载放到第一次任务调用之时
+  const [] = await MyTextToSpeechPipeline.getInstance(x => {
+    self.postMessage(x)
+  })
 }
