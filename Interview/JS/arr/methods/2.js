b07385c2cd74590b413e8033a104107e54e667e2
kangyi <1729166956@qq.com>
2025-08-26 17:53:42 +0800

    数组方法完整学习

---
+++ Interview/JS/arr/methods/2.js
@@ -0,0 +1,15 @@
+let arr = [3, 1, 2]     // 修改原数组
+
+// console.log(arr.sort(), arr)
+// >=0 调换 升序
+// console.log(arr.sort((a,b)=>a-b))
+// console.log(arr.sort((a,b)=>b-a))
+
+// 默认按字典进行排序
+// console.log([10, 1, 20, 3, 5].sort())
+
+// console.log(arr.reverse(), arr)
+
+//  arr.fill(value,start,end)
+// es6
+console.log(arr.fill(0, 1, 3), arr)
