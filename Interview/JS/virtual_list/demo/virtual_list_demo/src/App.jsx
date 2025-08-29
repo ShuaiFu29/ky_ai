9ec056ffb1be63e6caaecb96411f0f7e57866945
kangyi <1729166956@qq.com>
2025-08-29 20:51:16 +0800

    虚拟列表学习

--- Interview/JS/virtual_list/demo/virtual_list_demo/src/App.jsx
+++ Interview/JS/virtual_list/demo/virtual_list_demo/src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import VirtualList from './components/VirtualList'
+
+const generateData = (count) =>
+  Array.from({ length: count }, (_, i) => ({
+    id: i,
+    name: `Item ${i}`,
+    description:
+      `This is item number ${i}, rendered with virtual scrolling`
+  }))
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+  const data = generateData(100000);
+  const renderItem = (item, index) => (
+    <div
+      style={{
+        padding: '10px',
+        borderBottom: '1px solid #eee',
+        backgroundColor: index % 2 === 0 ? '#f9f9f9' : '#fff'
+      }}
+    >
+      <strong>[{index}]</strong> {item.name}
+      <p
+        style={{ margin: '4px 0', fontSize: '0.9em', color: '#666' }}
+      >
+        {item.description}
       </p>
-    </>
+    </div>
+  )
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Virtual List With 100000 Items</h1>
+      <p>Smooth scrolling with Virtualization</p>
+      {/* 固定高度的虚拟列表 */}
+      <VirtualList
+        data={data}
+        height={window.innerHeight - 100}
+        itemHeight={80}
+        renderItem={renderItem}
+        overscan={3}
+      />
+    </div>
   )
 }
 
--- Interview/JS/virtual_list/readme.md
+++ Interview/JS/virtual_list/readme.md
@@ -1,6 +1,13 @@
 # 虚拟列表
 
 - 一次性给你10 万条数据插入页面，你会怎么做？
+    - 时间分片
+    - 虚拟列表
+        没有必要，只需要加载视窗？
+        #contanier 高度 scroll
+        offsetTop 偏移量  transform translateY
+        index start -> end 20
+        item height
 - eventLoop
     - JS 单线程的开销肯定很大 页面的卡顿
     - 页面渲染开销也大
